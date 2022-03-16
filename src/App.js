0f8bcb1843872cc61756c8d96d3240425d6f2be5
Sydney Mercier <sydney.mercier91@gmail.com>
2022-03-15 20:07:32 -0500

    created single pokecard component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Pokecard 
+					id={4}
+					name="Charmander"
+					type="fire"
+					exp={62}
+				/>
+			</div>
+		);
+	}
+}
+
+export default App;
